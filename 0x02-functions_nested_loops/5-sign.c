fef68b66ab5c41f755839db723faabf214ec2c66
Alabi-williams samuel <49067163+willspot@users.noreply.github.com>
2023-06-21 07:16:07 +0800

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
   * print_sign - it prints the sign of a number
   * @n: The number to be checked
   *
-  * Return: 1 for positive number, -1 for negative number or zero for anything else
+  * Return: -1 for negative num, 1 for positive num,  or zero for anything else
   */
 int print_sign(int n)
 {
