1e08fa7a2af9fcbe9a5a91c1e0495615f705c592
Alabi-williams samuel <49067163+willspot@users.noreply.github.com>
2023-06-21 06:42:04 +0800

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+  * main : entry point
+  *
+  * Return : always 0
+  */
+int main(void)
+{
+	_putchar('_');
+	_putchar('P');
+	_putchar('U');
+	_putchar('T');
+	_putchar('C');
+	_putchar('H');
+	_putchar('A');
+	_putchar('R');
+	_putchar('\n');
+
+	return (0);
+}
