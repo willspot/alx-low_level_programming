5cfa46904085ae2131c634378b0e757e045fc5f7
Alabi-williams samuel <49067163+willspot@users.noreply.github.com>
2023-07-05 18:42:48 -0400

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: string
+ *
+ * Return: 1 On success.
+ * On error, -1 is returned.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
