78f52563d8a3eb9fcc3381e62a134c91b14cf591
Alabi-williams samuel <49067163+willspot@users.noreply.github.com>
2023-07-31 10:09:40 -0400

    Create 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - A function that gets a node of list at specific index
+ * @head: A pointer to listint_t structure
+ * @index: The index of the node
+ * Return: NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
