57067c753191c3610d369228478d744649df639e
Alabi-williams samuel <49067163+willspot@users.noreply.github.com>
2023-09-05 02:19:31 +0800

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 /**
- * read_textfile - prints text from a file
  * Prototype: ssize_t read_textfile(const char *filename, size_t letters);
+ * read_textfile - prints text from a file
  * @filename: name of the file
  * @letters: number of characters to read
- *
  * Return: actual number of letters read, if the file can not be opened or read, return 0
  * if filename is NULL return 0
  * if write fails or does not write the expected amount of bytes, return 0
